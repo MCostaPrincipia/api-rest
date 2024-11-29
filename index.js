e8ae74206b98f95393f21e9efd115bd51503fd68
Michel Costa <michel.costa@principia.net>
2024-11-29 03:02:15 -0300

    commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//Configuração incial
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+
+// Forma de ler JSON no Express (Middlewares)
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+
+// Rotas da API
+const personRoutes = require('./routes/personRoutes')
+app.use('/person', personRoutes)
+
+
+// Rota Teste / Endpoint
+
+app.get('/', (req, res) => {
+    
+    // Mostrar requisição
+    res.json({ message: 'Oi, Express!'})
+    
+})
+
+const DB_USER = process.abort.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
+mongoose
+    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.98iyq.mongodb.net/?retryWrites=true&w=majority&appName=APICluster`)
+        .then(() => {
+            console.log('Conectou ao banco!')
+            app.listen(3000)
+  })
+    .catch((err) => console.log(err))
